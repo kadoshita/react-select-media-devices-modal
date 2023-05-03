13441478c3a8c78e0dae6ec89d57d5ccff159252
kadoshita <sublimer.me@gmail.com>
2023-05-03 11:49:44 +0900

    Update components style

--- src/SelectMediaDevicesModal/components/deviceList/index.tsx
+++ src/SelectMediaDevicesModal/components/deviceList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DeviceItem from '../deviceItem';
+import s from './style.module.css';
 
 interface DeviceListProps {
     devices: MediaDeviceInfo[];
@@ -17,14 +18,14 @@ const DeviceList = ({ devices, label, onChange }: DeviceListProps) => {
     const selectId = `device-select-${label.toLowerCase().replace(/\s/g, '-')}`;
 
     return (
-        <>
+        <div className={s.deviceList}>
             <label htmlFor={selectId}>{label}</label>
-            <select id={selectId} onChange={handleChange}>
+            <select className={s.select} id={selectId} onChange={handleChange}>
                 {devices.map((d, i) => (
                     <DeviceItem value={d.deviceId} name={d.label} key={i}></DeviceItem>
                 ))}
             </select>
-        </>
+        </div>
     );
 };
 
