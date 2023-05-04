695b831ca0e9e1dbf7732d8707b6ceb2b25b8725
kadoshita <sublimer.me@gmail.com>
2023-05-04 11:44:30 +0900

    Update to rename SelectMediaDevicesModal test file

--- __tests__/SelectMediaDevicesModal.test.tsx
+++ __tests__/SelectMediaDevicesModal.test.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import '@testing-library/jest-dom';
 import { render, screen, waitFor } from '@testing-library/react';
-import { vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 import SelectMediaDevicesModal from '../src/SelectMediaDevicesModal';
 import { useGetDevices } from '../src/hooks/useGetDevices';
