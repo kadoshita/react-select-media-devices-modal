24bf113b805c1fd45704b35a360aad3d0043df90
kadoshita <sublimer.me@gmail.com>
2024-06-24 17:47:56 +0900

    Update to migrate @vitest/coverage-c8 to @vitest/coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [react()],
     test: {
         include: ['**/*.test.ts', '**/*.test.tsx'],
-        exclude: ['**/e2e/*.test.ts'],
+        exclude: ['**/e2e/*.test.ts', 'node_modules/**'],
         environment: 'jsdom',
         globals: true,
         setupFiles: ['__tests__/setup.ts'],
